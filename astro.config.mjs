2d61c3b29479d96c2f25f8828ba755439159bde4
Samuel Sinaga <samuelfrans672@gmail.com>
2026-03-23 01:58:52 +0700

    fix base URL for Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,28 @@
-import { defineConfig } from 'astro/config';
-import { fileURLToPath } from 'url';
+import { defineConfig } from "astro/config";
+import { fileURLToPath } from "url";
 
 export default defineConfig({
-  site: 'https://aarus2709.me',
-  base: '/',
-  output: 'static',
+  site: "https://sfrans21.github.io",
+  base: "/portofolio-v2",
+  output: "static",
   compressHTML: true,
   build: {
-    inlineStylesheets: 'auto',
+    inlineStylesheets: "auto",
   },
   vite: {
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
-        '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
-        '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
-        '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+        "@components": fileURLToPath(
+          new URL("./src/components", import.meta.url),
+        ),
+        "@layouts": fileURLToPath(new URL("./src/layouts", import.meta.url)),
+        "@styles": fileURLToPath(new URL("./src/styles", import.meta.url)),
+        "@utils": fileURLToPath(new URL("./src/utils", import.meta.url)),
       },
     },
     build: {
-      cssMinify: 'lightningcss',
+      cssMinify: "lightningcss",
       rollupOptions: {
         output: {
           manualChunks: undefined,
